640483e3a9246386102e19bda99cc4bd071197b6
lichong21 <1774756683@qq.com>
2023-09-12 17:15:20 +0800

    feat:调整组件导入路径

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import elementUI from 'element-ui'
-import './styles/element-variables.scss'
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.use(elementUI)
 
 // ------------------------------分割线------------------------------
-import Select from '../src/element-ui/select'
-import TableList from '../src/element-ui/table-list'
-import TextEllipsis from '../src/element-ui/text-ellipsis'
-import DateRangePicker from '..src/element-ui/date-range-picker'
-import SearchForm from '..src/element-ui/search-form'
-import CacheInput from '../src/element-ui/cache-input'
+import Select from './src/element-ui/select'
+import TableList from './src/element-ui/table-list'
+import TextEllipsis from './src/element-ui/text-ellipsis'
+import DateRangePicker from '.src/element-ui/date-range-picker'
+import SearchForm from '.src/element-ui/search-form'
+import CacheInput from './src/element-ui/cache-input'
+
 
 const components = [
 	DateRangePicker,
